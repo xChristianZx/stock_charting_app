74e0396bd9e243eaa95dff70ab4d3bc8a324aeb1
Christian Zenaty <cazenaty@yahoo.com>
2018-01-17 16:58:07 -0800

    added client

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("HELLO THERE");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serving on Port ${PORT}`);
