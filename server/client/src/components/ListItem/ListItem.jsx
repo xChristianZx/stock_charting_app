4c9f4fb11249ecaef4818b54091e963a7637e3c9
Christian Zenaty <cazenaty@yahoo.com>
2018-01-19 12:40:33 -0800

    updated watchlist and listitem

--- server/client/src/components/ListItem/ListItem.jsx
+++ server/client/src/components/ListItem/ListItem.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./ListItem.css";
 
-const ListItem = () => {
-    return (
-        <li>
-            
-        </li>
-    );
+const ListItem = props => {
+  return (
+    <tr className="item-row">
+      <td className="ticker">GE</td>
+      <td className="price">100</td>
+    </tr>
+  );
 };
 
 export default ListItem;
--- server/client/src/containers/WatchList/WatchList.jsx
+++ server/client/src/containers/WatchList/WatchList.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import "./WatchList.css";
+import ListItem from "../../components/ListItem/ListItem";
 
 class WatchList extends Component {
   render() {
     return (
       <div className="watchlist-container">
         <table className="list">
-          <tr>
-            <td className="ticker">Ticker</td>
-            <td className="price">Price</td>
-          </tr>
+          <thead>
+            <tr className="header-row">
+              <th className="ticker">Ticker</th>
+              <th className="price">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="body-row">
+              <td className="ticker">AAPL</td>
+              <td className="price">300</td>
+            </tr>
+            <ListItem />
+          </tbody>
         </table>
       </div>
     );
