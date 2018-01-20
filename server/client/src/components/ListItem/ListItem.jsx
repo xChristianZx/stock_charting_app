06ba8620f8fb9084fe6e8fe8a8b4d32f51877efd
Christian Zenaty <cazenaty@yahoo.com>
2018-01-19 19:43:59 -0800

    added axios and data fetching

--- server/client/src/components/ListItem/ListItem.jsx
+++ server/client/src/components/ListItem/ListItem.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./ListItem.css";
 
-const ListItem = ({ data }) => {
-  const list = data.map(item => {
+const ListItem = props => {
+  const data = props.data;
+  const fetchTicker = props.fetchTicker;
+
+  const list = data.map((item, i) => {
     return (
-      <tr className="item-row">
+      <tr
+        className="item-row"
+        key={i}
+        onClick={fetchTicker.bind(this, item.ticker, i)}
+      >
         <td className="ticker">{item.ticker}</td>
         <td className="price">{item.price}</td>
       </tr>
--- server/client/src/containers/WatchList/WatchList.jsx
+++ server/client/src/containers/WatchList/WatchList.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 import "./WatchList.css";
 import ListItem from "../../components/ListItem/ListItem";
+import Axios from "axios";
+import API_KEY from "../../config/keys";
 
 class WatchList extends Component {
   state = {
-    data: [{ ticker: "AAPL", price: 300 }, { ticker: "GE", price: 40 }]
+    data: [],
+    focus: null
+  };
+
+  componentDidMount() {
+    const data = [{ ticker: "AAPL", price: 300 }, { ticker: "GE", price: 40 }];
+    this.setState({ data: data });
+  }
+
+  Axios = ticker => {
+    const baseUrl =
+      "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY";
+    const options = {
+      params: {
+        symbol: ticker,
+        apikey: API_KEY
+      }
+    };
+
+    Axios
+      .get(baseUrl, options)
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+
+  fetchTicker = (ticker, id) => {
+    this.Axios(ticker);
+    this.setState({ focus: id });
   };
 
   render() {
@@ -18,7 +47,7 @@ class WatchList extends Component {
             </tr>
           </thead>
           <tbody>
-            <ListItem data={this.state.data} />
+            <ListItem data={this.state.data} fetchTicker={this.fetchTicker} />
           </tbody>
         </table>
       </div>
