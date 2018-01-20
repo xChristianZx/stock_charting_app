f937fa4b28cd2874c03c2788a9cf7cee47a686c5
Christian Zenaty <cazenaty@yahoo.com>
2018-01-20 09:54:37 -0800

    lifted state and switched to IEX datafeed

--- server/client/src/App.jsx
+++ server/client/src/App.jsx
@@ -2,12 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 import WatchList from "./containers/WatchList/WatchList";
 // import Charts from "./containers/Charts/Charts";
+import Axios from "axios";
 
 class App extends Component {
+  state = {
+    stocksArray: [],
+    focus: null
+  };
+
+  componentDidMount() {
+    const stocksArray = [{ ticker: "AAPL" }, { ticker: "GE" }];
+    this.setState({ stocksArray });
+  }
+
+  Axios = ticker => {
+    const baseUrl = "https://api.iextrading.com/1.0/stock/";
+    const compUrl = `${baseUrl}${ticker}/chart/1y`;
+
+    Axios.get(compUrl)
+      .then(payload => {
+        console.log(ticker, payload);
+      })
+      .catch(err => console.log(err));
+  };
+
+  fetchTicker = (ticker, id) => {
+    this.Axios(ticker);
+    this.setState({ focus: id });
+  };
+
   render() {
     return (
       <div className="App">
-        <WatchList />
+        <WatchList
+          data={this.state.stocksArray}
+          fetchTicker={this.fetchTicker}
+        />
         {/* <Charts /> */}
       </div>
     );
--- server/client/src/containers/WatchList/WatchList.jsx
+++ server/client/src/containers/WatchList/WatchList.jsx
@@ -1,41 +1,8 @@
 import React, { Component } from "react";
 import "./WatchList.css";
 import ListItem from "../../components/ListItem/ListItem";
-import Axios from "axios";
-import API_KEY from "../../config/keys";
 
 class WatchList extends Component {
-  state = {
-    data: [],
-    focus: null
-  };
-
-  componentDidMount() {
-    const data = [{ ticker: "AAPL", price: 300 }, { ticker: "GE", price: 40 }];
-    this.setState({ data: data });
-  }
-
-  Axios = ticker => {
-    const baseUrl =
-      "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY";
-    const options = {
-      params: {
-        symbol: ticker,
-        apikey: API_KEY
-      }
-    };
-
-    Axios
-      .get(baseUrl, options)
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  };
-
-  fetchTicker = (ticker, id) => {
-    this.Axios(ticker);
-    this.setState({ focus: id });
-  };
-
   render() {
     return (
       <div className="watchlist-container">
@@ -47,7 +14,7 @@ class WatchList extends Component {
             </tr>
           </thead>
           <tbody>
-            <ListItem data={this.state.data} fetchTicker={this.fetchTicker} />
+            <ListItem data={this.props.data} fetchTicker={this.props.fetchTicker} />
           </tbody>
         </table>
       </div>
