fea3fd55b37d6ae1d45e9cc9dd8b1e9a95d39fd8
Christian Zenaty <cazenaty@yahoo.com>
2018-02-08 21:15:03 -0800

    pkg's update and added react-icons

--- server/client/src/components/NavBar/NavBar.js
+++ server/client/src/components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./NavBar.css";
+import FaLine from "react-icons/lib/fa/line-chart";
 
 const NavBar = () => {
   return (
     <div className="navbar-container">
       <div className="logo-wrapper">
-        <h2 className="logo-name">ChartIt</h2>
+        <h1 className="logo-name">
+          <FaLine />   ChartIt
+        </h1>
       </div>
     </div>
   );
