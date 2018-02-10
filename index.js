d3a89106a39362391c2c8abcd76bf6b30248504e
Christian Zenaty <cazenaty@yahoo.com>
2018-02-09 16:39:29 -0800

    working on heroku problem

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ mongoose.connect(keys.mongoURI), { useMongoClient: true };
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.get("/", (req, res) => {
-//   res.json([{ express: "was here" }, { twelve: "12" }]);
-// });
+// app.get("/", (req, res) => {});
 
 //Server to Client Connection
 wsServer.on("connection", ws => {
@@ -94,18 +92,18 @@ wsServer.on("connection", ws => {
   });
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   //Express will serve up production assets
-//   //like our main.js file, or main.css file
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets
+  //like our main.js file, or main.css file
+  app.use(express.static("client/build"));
 
-//   // Express will serve up the index.html file
-//   // if it doesn't recognize the route
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
