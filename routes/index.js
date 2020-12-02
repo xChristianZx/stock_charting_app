6daa8df703160810637a705ae65c22fa5e0114e3
Christian Zenaty <cazenaty@yahoo.com>
2020-12-01 19:59:03 -0800

    Variable rename

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,21 @@ const baseSandboxUrl = 'https://sandbox.iexapis.com/stable/';
 
 const router = express.Router();
 
-router.get('/tickervalidation', async (req, res) => {
+router.get('/currentstats', async (req, res) => {
   const { ticker } = req.query;
 
-  const compUrl = `${baseCloudUrl}tops/last?symbols=${ticker}&token=${keys.IEX_CLOUD_TOKEN}`;
+  const compUrl = `${baseSandboxUrl}stock/${ticker}/stats?token=${keys.IEX_SANDBOX_TOKEN}`;
 
   const iexRes = await axios.get(compUrl);
   const { data } = iexRes;
 
   if (data.length < 1) {
-    return res.status(404).json({ msg: 'Symbol does not exist', data });
+    return res
+      .status(404)
+      .json({ msg: 'There is no data for this symbol at this times' });
   }
 
-  res.status(201).json({ msg: `${ticker}: Valid ticker symbol`, data });
+  res.status(201).json({ data });
 });
 
 router.get('/watchlist/validation', async (req, res) => {
