12ef075bed06bae74cf95f01318eee098fe0631d
Christian Zenaty <cazenaty@yahoo.com>
2020-12-02 10:48:49 -0800

    Ooops, forgot the prod env variables

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI_PROD
+  mongoURI: process.env.MONGO_URI_PROD,
+  IEX_CLOUD_TOKEN: process.env.IEX_CLOUD_TOKEN,
+  IEX_SANDBOX_TOKEN: process.env.IEX_SANDBOX_TOKEN,
 };
