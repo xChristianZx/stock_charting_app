1812ef666ede037f5ed28e5e4d26035b12876f11
Christian Zenaty <cazenaty@yahoo.com>
2020-12-01 19:58:34 -0800

    Update watchlist routing

--- routes/index.js
+++ routes/index.js
@@ -23,21 +23,35 @@ router.get('/tickervalidation', async (req, res) => {
   res.status(201).json({ msg: `${ticker}: Valid ticker symbol`, data });
 });
 
-router.get('/currentstats', async (req, res) => {
+router.get('/watchlist/validation', async (req, res) => {
   const { ticker } = req.query;
 
-  const compUrl = `${baseSandboxUrl}stock/${ticker}/stats?token=${keys.IEX_SANDBOX_TOKEN}`;
+  const compUrl = `${baseCloudUrl}tops/last?symbols=${ticker}&token=${keys.IEX_CLOUD_TOKEN}`;
 
   const iexRes = await axios.get(compUrl);
   const { data } = iexRes;
 
   if (data.length < 1) {
-    return res
-      .status(404)
-      .json({ msg: 'There is no data for this symbol at this times' });
+    return res.status(404).json({ msg: 'Symbol does not exist', data });
   }
 
-  res.status(201).json({ data });
+  res.status(201).json({ msg: `${ticker}: Valid ticker symbol`, data });
+});
+
+router.get('/watchlist/data', async (req, res) => {
+  const { tickerArr } = req.query;
+  console.log('tickerArr', tickerArr);
+
+  const compUrl = `${baseCloudUrl}tops/last?symbols=${tickerArr}&token=${keys.IEX_CLOUD_TOKEN}`;
+
+  const iexRes = await axios.get(compUrl);
+  const { data } = iexRes;
+
+  if (data.length < 1) {
+    return res.status(404).json({ msg: 'There is no data at this times' });
+  }
+
+  res.status(200).json({ data });
 });
 
 module.exports = router;
